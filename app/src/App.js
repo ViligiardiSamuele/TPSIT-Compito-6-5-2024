3a9637e35b6b29cf78da63a9ce266fc35f4c42bd
lai2-24 <lai2-24@itismeucci.com>
2024-05-06 10:00:20 +0200

    wip

--- app/src/App.js
+++ app/src/App.js
@@ -10,12 +10,19 @@ document.body.setAttribute("data-bs-theme", "dark");
 function App() {
   const [partita, setPartita] = useState("");
   const [numero, setNumero] = useState("");
+  const [messaggio, setMessaggio] = useState("");
 
   const [caricamento, setInCaricamento] = useState(false);
   const [indovinato, setIndovinato] = useState(false);
 
+  useEffect(() => {
+    nuovaPartita();
+  }, []);
+
   async function nuovaPartita() {
     setInCaricamento(true);
+    setIndovinato(false);
+    setNumero("");
     const response = await fetch(`http://localhost:8080/partita`, {
       method: "POST",
     });
@@ -25,22 +32,30 @@ function App() {
 
   async function tentativo() {
     setInCaricamento(true);
-    const response = await fetch(`http://localhost:8080/partita/${partita.id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id: partita.id, numero: numero }),
-    });
-    response = await response.json();
-    if(response['risultato'] = -1){
-
-    } else if (response['risultato'])
+    setNumero("");
+    const response = await fetch(
+      `http://localhost:8080/partita/${partita.id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: partita.id, numero: numero }),
+      }
+    );
+    let risposta = await response.json();
+    partita.tentativi = risposta['tentativi'];
+    if ((risposta['risultato'] < 0)) {
+      setMessaggio("Il numero inserito è troppo PICCOLO");
+    } else if ((risposta['risultato'] > 0)) {
+      setMessaggio("Il numero inserito è troppo GRANDE");
+    } else {
+      setIndovinato(true);
     }
     setInCaricamento(false);
   }
 
-  useEffect(() => {
-    nuovaPartita();
-  }, []);
+  function HandlerNumero(e) {
+    setNumero(e.target.value);
+  }
 
   return (
     <div className="App">
@@ -60,7 +75,8 @@ function App() {
             ) : (
               <>
                 <p>Inserisci un numero tra 1 e 100:</p>
-                <FormControl className="w-50 m-1 mx-auto"></FormControl>
+                <p>{messaggio}</p>
+                <FormControl name="numero" value={numero} onChange={HandlerNumero} type="number" min="1" max="100" className="w-50 m-1 mx-auto"></FormControl>
                 <Button className="w-50 m-1 mx-auto" onClick={tentativo}>
                   Invia
                 </Button>
